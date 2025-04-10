92938a3f7c000036db490c7723279baa20915a9a
ingarskrafts <ingarskrafts@gmail.com>
2025-04-10 05:49:49 -0400

    countries

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,4 @@
+// Fetching the data from travel_recommendation_api.json
 fetch('travel_recommendation_api.json')
     .then(response => {
         if (!response.ok) {
@@ -7,5 +8,15 @@ fetch('travel_recommendation_api.json')
     })
     .then(data => {
         console.log('Fetched Travel Data:', data);
-        
+
+        // Display Countries
+        console.log('Countries');
+        data.countries.forEach(country => {
+            console.log(`Country: ${country.name}`);
+            country.cities.forEach(city => {
+              console.log(`City: ${city.name}`);
+              console.log(`Image: ${city.imageUrl}`);
+              console.log(`Description: ${city.description}`);
+            });
+          });
     })
